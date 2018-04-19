68c0257c275dec864b67fed3f9823d8169b3e331
lipin <1218683832@qq.com>
2018-04-19 17:50:55 +0800

    lipin

--- module_share/src/main/java/com/mrrun/module_share/wx/wxapi/WXEntryActivity.java
+++ module_share/src/main/java/com/mrrun/module_share/wx/wxapi/WXEntryActivity.java
@@ -18,7 +18,7 @@ import com.tencent.mm.opensdk.openapi.WXAPIFactory;
  * @author lipin
  * @version 1.0
  * @date 2018/04/13
- *
+ * <p>
  * 微信分析回调
  * 新建wxapi包并新建WXEntryActivity类，注意名字和包名必须是wxapi和WXEntryActivity
  */
@@ -26,11 +26,11 @@ public class WXEntryActivity extends Activity implements IWXAPIEventHandler {
     /**
      * 微信分享getType为0
      */
-    public  int WX_SHARE = 0;
+    public int WX_SHARE = 0;
     /**
      * 微信登录getType为1
      */
-    public  int WX_LOGIN = 1;
+    public int WX_LOGIN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +42,7 @@ public class WXEntryActivity extends Activity implements IWXAPIEventHandler {
 
     /**
      * 微信请求结果回调处理
+     *
      * @param baseReq
      */
     @Override
@@ -51,6 +52,7 @@ public class WXEntryActivity extends Activity implements IWXAPIEventHandler {
 
     /**
      * 微信响应结果回调处理
+     *
      * @param baseResp
      */
     @Override
@@ -59,20 +61,32 @@ public class WXEntryActivity extends Activity implements IWXAPIEventHandler {
         if (baseResp.getType() == 1) {
             Debug.D("------------登陆回调------------");
             SendAuth.Resp resp = (SendAuth.Resp) baseResp;
-            Debug.D("------------登陆回调的结果------------：" +  new Gson().toJson(resp));
+            Debug.D("------------登陆回调的结果------------：" + new Gson().toJson(resp));
+            switch (resp.errCode) {
+                case BaseResp.ErrCode.ERR_OK:// 微信登录成功
+                    String code = String.valueOf(resp.code);
+                    // 获取用户信息
+                    // getAccessToken(code);
+                    break;
+                case BaseResp.ErrCode.ERR_AUTH_DENIED:// 微信登录拒绝授权
+                    Toast.makeText(WXEntryActivity.this, "微信登录拒绝授权", Toast.LENGTH_LONG).show();
+                    break;
+                case BaseResp.ErrCode.ERR_USER_CANCEL:// 微信登录取消
+                    Toast.makeText(WXEntryActivity.this, "微信登录取消", Toast.LENGTH_LONG).show();
+                    break;
+                default:
+                    break;
+            }
         } else if (baseResp.getType() == 0) {// 分享成功回调
             Debug.D("------------分享回调------------");
             switch (baseResp.errCode) {
-                case BaseResp.ErrCode.ERR_OK:
-                    // 分享成功
+                case BaseResp.ErrCode.ERR_OK: // 分享成功
                     Toast.makeText(WXEntryActivity.this, "分享成功", Toast.LENGTH_LONG).show();
                     break;
-                case BaseResp.ErrCode.ERR_USER_CANCEL:
-                    // 分享取消
+                case BaseResp.ErrCode.ERR_USER_CANCEL:// 分享取消
                     Toast.makeText(WXEntryActivity.this, "分享取消", Toast.LENGTH_LONG).show();
                     break;
-                case BaseResp.ErrCode.ERR_AUTH_DENIED:
-                    // 分享拒绝
+                case BaseResp.ErrCode.ERR_AUTH_DENIED:// 分享拒绝
                     Toast.makeText(WXEntryActivity.this, "分享拒绝", Toast.LENGTH_LONG).show();
                     break;
             }
