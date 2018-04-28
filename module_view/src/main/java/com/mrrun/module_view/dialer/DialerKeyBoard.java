9509da8f8f9c1dbb2e064c8ec584f8dc0caea8c3
lipin <1218683832@qq.com>
2018-04-28 14:40:39 +0800

    lipin

---
+++ module_view/src/main/java/com/mrrun/module_view/dialer/DialerKeyBoard.java
@@ -0,0 +1,8 @@
+package com.mrrun.module_view.dialer;
+
+public class DialerKeyBoard {
+}
+
+class DialerKeyRow {
+
+}
--- module_view/src/main/java/com/mrrun/module_view/dialer/SoftKeyBoard.java
+++
@@ -1,8 +0,0 @@
-package com.mrrun.module_view.dialer;
-
-public class SoftKeyBoard {
-}
-
-class SoftKeyRow {
-
-}
