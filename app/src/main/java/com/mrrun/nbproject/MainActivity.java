a1812609b58573a4f1e7c68f6163951cc5459613
lipin <1218683832@qq.com>
2018-04-28 18:03:23 +0800

    lipin

--- app/src/main/java/com/mrrun/nbproject/MainActivity.java
+++ app/src/main/java/com/mrrun/nbproject/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         uiShare();
         uiSearchView();
-        uiDialer();
+//        uiDialer();
         uiXmlData();
     }
 
