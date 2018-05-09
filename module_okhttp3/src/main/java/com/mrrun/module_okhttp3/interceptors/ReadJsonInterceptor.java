7c255e0a30d291005503c8155771a92bd9cc2f2f
lipin <1218683832@qq.com>
2018-05-09 12:43:27 +0800

    加入okhttp3示例

---
+++ module_okhttp3/src/main/java/com/mrrun/module_okhttp3/interceptors/ReadJsonInterceptor.java
@@ -0,0 +1,36 @@
+package com.mrrun.module_okhttp3.interceptors;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.MediaType;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+/**
+ * 网络拦截器
+ * 拦截读取Json格式数据
+ */
+public class ReadJsonInterceptor implements Interceptor {
+
+    MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        System.out.println("ReadJsonInterceptor 拦截读取Json格式数据");
+        Response response =  chain.proceed(chain.request());
+        Response newResponse = null;
+        MediaType mediaType = response.body().contentType();
+        if (mediaType.type().equals(JSON.type())){// Json格式数据
+            System.out.println("约定的Json格式数据");
+            String content = response.body().string();
+            System.out.println("内容是:" + content);
+            newResponse = response.newBuilder()
+                    .body(ResponseBody.create(mediaType, content))
+                    .build();
+        } else {
+            System.out.println("非约定的Json格式数据");
+        }
+        return newResponse != null ? newResponse : response;
+    }
+}
--- module_okhttp3/src/test/java/com/mrrun/module_okhttp3/InterceptorUnitTest.java
+++ module_okhttp3/src/test/java/com/mrrun/module_okhttp3/InterceptorUnitTest.java
@@ -2,6 +2,7 @@ package com.mrrun.module_okhttp3;
 
 import com.mrrun.module_okhttp3.interceptors.LoggingInterceptor;
 import com.mrrun.module_okhttp3.interceptors.NetInterceptor;
+import com.mrrun.module_okhttp3.interceptors.ReadJsonInterceptor;
 
 import org.junit.Test;
 
@@ -37,6 +38,25 @@ public class InterceptorUnitTest {
         // 客户端
         OkHttpClient client = new OkHttpClient.Builder()
                 .addNetworkInterceptor(new NetInterceptor())// 网络拦截器
+                .build();
+        // 请求头数据
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+        try {
+            client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void readJsonInterceptorTest(){
+        String url = "http://baike.baidu.com/api/openapi/BaikeLemmaCardApi?scope=103&format=json&appid=379020&bk_key=%E9%93%B6%E9%AD%82&bk_length=600";
+        // 客户端
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addNetworkInterceptor(new ReadJsonInterceptor())// Application拦截器
                 .build();
         // 请求头数据
         Request request = new Request.Builder()
